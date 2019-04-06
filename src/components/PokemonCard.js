fb218da95a3fce89a1eca20594b12144925783fe
github username <github email address>
2019-04-06 14:16:02 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,33 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    clicked: false
+  }
+
+  clickHandler = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+
   render() {
+    let hp = this.props.pokemonObj.stats.find((stat) => {
+      return (stat['name']==='hp')
+    })
+
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div className="image" onClick={this.clickHandler}>
+            {this.state.clicked? <img alt="oh no!" src={this.props.pokemonObj.sprites.back} /> : <img alt="oh no!" src={this.props.pokemonObj.sprites.front} />}
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemonObj.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp['value']}
             </span>
           </div>
         </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,16 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
   render() {
+    console.log(this.props);
+    let pokemonArr = this.props.pokemons.map((pokemon) => {
+      return <PokemonCard pokemonObj={pokemon} key={pokemon.name}/>
+    })
+
     return (
       <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
+        { pokemonArr }
       </Card.Group>
     )
   }
