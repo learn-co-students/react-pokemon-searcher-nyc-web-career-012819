d4846c33b8393f991184d954c43010e5d640a91e
dren39 <dren391@gmail.com>
2019-04-07 17:40:21 -0400

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,26 @@ class PokemonForm extends React.Component {
     }
   }
 
+  changeHandler = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.postNewPokemon(this.state)
+    this.setState({name:'', hp:'', frontUrl:'', backUrl:''})
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.changeHandler} value={this.state.name}/>
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.changeHandler} value={this.state.hp}/>
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.changeHandler} value={this.state.frontUrl}/>
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" onChange={this.changeHandler} value={this.state.backUrl}/>
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
