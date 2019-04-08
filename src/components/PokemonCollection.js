7865cd366e3a6c6baa52f1bc9a6bf2eb4f81c229
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-04-08 08:50:41 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,10 +3,27 @@ import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
+
   render() {
+    console.log("THE CURRENT LIST OF POKEMONS:", this.props.pokemons);
+
+    const pokemons = this.props.pokemons.map(pokemonObj => {
+      let {name, sprites: {front, back}, stats } = pokemonObj
+      let hp = stats.find(stat => stat.name === "hp").value
+
+      return <PokemonCard
+               key={pokemonObj.id}
+               name={name}
+               front={front}
+               back={back}
+               hp={hp}
+            />
+    })
+
     return (
       <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
+        {pokemons}
       </Card.Group>
     )
   }
