fe99f9b8ad2df158cdf88249f7afab3587119e5f
faizaniq <github email address>
2019-04-09 20:37:02 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,28 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  state = {
+    clicked: true
+  }
+
+  clickHandler = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+
   render() {
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div onClick={this.clickHandler} className="image">
+            <img src={this.state.clicked? this.props.pokemon.sprites.front: this.props.pokemon.sprites.back} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.stats[5].value}
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,40 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemons: [],
+    search: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemon => this.setState({pokemons:pokemon}))
+
+  }
+
+  changeHandler = (event) => {
+    this.setState({search: event.target.value})
+  }
+
+
+  submitHandler = (props) => {
+    let newArray = [...this.state.pokemons, props]
+    this.setState({pokemons: newArray})
+  }
+
   render() {
+    let search = this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.search))
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.changeHandler} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={search}/>
         <br />
-        <PokemonForm />
+        <PokemonForm submitHandler={this.submitHandler}/>
       </div>
     )
   }
